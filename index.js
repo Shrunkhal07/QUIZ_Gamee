e7174625b4d9c87fca6b9fef2864398eeed90267
Shrunkhal Wankhede <83139835+Shrunkhal07@users.noreply.github.com>
2022-12-21 11:44:57 +0000

    Changed the topic of the quiz.

--- index.js
+++ index.js
@@ -4,69 +4,78 @@ var readlineSync = require("readline-sync");
 
 //console.log(chalk.pink('Hello world'));
 
-var score =0;
+var score = 0;
 
 var userName = readlineSync.question("Please tell me your name ");
-console.log("Welcome to Naruto quiz!!",userName);
+console.log("Welcome to my Quiz!!", userName);
 
-          // loop
+// loop
 
-function play(question, answer)
-{
+function play(question, answer) {
   var userAnswer = readlineSync.question(question);
 
-  if(userAnswer == answer){
+  if (userAnswer == answer) {
     console.log("Correct answer!");
-    score = score+1;
-    
+    score = score + 1;
+
   }
-  else{
+  else {
     console.log("Opps!....the answer is incorrect")
   }
-  
-  console.log("Your Score is: "+score);
+
+  console.log("Your Score is: " + score);
 
 }
-         //HIGHSCORE object
- var highScores = [{
-                 name: "Shrunkhal",
-                 score: 10,
-                  }];
-          //ARRAY + OBJECT
+//HIGHSCORE object
+var highScores = [{
+  name: "Shrunkhal",
+  score: 10,
+}];
+//ARRAY + OBJECT
 var quiz = [{
-           question: "What is naruto's favourite food? ",
-           answer:   "ramen"
-          }, {
-          question: "Who is naruto's crush? ",
-          answer: "sakura"
-          }, {
-          question: "Who is first Hokage? ",
-          answer: "hashirama"}
-           ];
-
-for(var i=0; i< quiz.length; i++)
-  {
-    var currentQuestion = quiz[i];
-    play(currentQuestion.question, currentQuestion.answer);
-    console.log("----------");
-  }
-
-console.log(userName+" Your Final Score is: "+ score);
-
-if(score == 0){
-  console.log("Aur Padhai kar chutiye");
-  console.log("------------");
-  
+  question: "What is my favourite food? ",
+  answer: "ramen"
+}, {
+  question: "Where do I live ? ",
+  answer: "nagpur"
+}, {
+  question: "What is my favourite anime? ",
+  answer: "naruto"
+},
+{
+  question: "What is my qualification?",
+  answer: "B.com"
+},
+{
+  question: "What is my favourite movie?",
+  answer: "Batman"
 }
-else{
+
+
+];
+
+for (var i = 0; i < quiz.length; i++) {
+  var currentQuestion = quiz[i];
+  play(currentQuestion.question, currentQuestion.answer);
+  console.log("----------");
+}
+
+console.log(userName + " Your Final Score is: " + score);
+
+if (score == 0) {
+  console.log("Correct Answer!!!");
+  console.log("------------");
+
+}
+else {
   console.log("Better Luck next time ");
   console.log("------------");
-  
+
 }
 
-for(var j=0; j< highScores.length; j++){
-var currentScore = highScores[j]; 
-console.log("Check out the high Score: " +currentScore.name + "-> " +currentScore.score);
-//console.log(currentScore.score);
+for (var j = 0; j < highScores.length; j++) {
+  var currentScore = highScores[j];
+  console.log("Check out the high Score: " + currentScore.name + "-> " + currentScore.score);
+  //console.log(currentScore.score);
 }
 
